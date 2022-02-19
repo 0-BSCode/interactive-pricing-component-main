f96609451eecce100c6e63be3a10e6c0b423a9de
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-02-19 11:31:45 +0800

    Fixed ID issue with dragbar

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const range = document.getElementById("myRange");
+const rangeMobile = document.getElementById("myRangeM");
+const rangeDesktop = document.getElementById("myRangeD");
 const trialBtn = document.querySelector(".card__trial");
 const trialTxt = document.querySelector(".card__trial-txt")
 
-range.oninput = function() {
+rangeMobile.oninput = function() {
+    const fillColor = "hsl(174, 77%, 80%)";
+    const bgColor = "hsl(224, 65%, 95%)";
+    let value = Math.floor((this.value - this.min) / (this.max - this.min) * 100);
+    this.style.background = `linear-gradient(to right, ${fillColor} 0%, ${fillColor} ${value}%, 
+                                            ${bgColor} ${value}%, ${bgColor} 100%)`;
+}
+
+rangeDesktop.oninput = function() {
     const fillColor = "hsl(174, 77%, 80%)";
     const bgColor = "hsl(224, 65%, 95%)";
     let value = Math.floor((this.value - this.min) / (this.max - this.min) * 100);
